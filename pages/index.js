5c285e8809f3d5fa468ec257c35631a66bf266d8
Marte Auer <marte_auer@hotmail.com>
2023-05-02 17:18:30 +0200

    add minor styling

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,15 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <>
-      <h2>TOTALLY UNHINGED CHESS</h2>
-      <h2>TOTALLY UNHINGED CHESS</h2>
-      <h2>TOTALLY UNHINGED CHESS</h2>
+      <h2>
+        TOTALLY <i>UNHINGED</i> CHESS
+      </h2>
+      <h2>
+        TOTALLY <i>UNHINGED</i> CHESS
+      </h2>
+      <h2>
+        TOTALLY <i>UNHINGED</i> CHESS
+      </h2>
       <div>
         <Link href="/mainboard">
           <button type="text">Go to Mainboard</button>
