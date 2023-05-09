f492b08947ac193c2a149f2a7d483ee6805d7ad3
Marte Auer <marte_auer@hotmail.com>
2023-05-09 18:38:26 +0200

    works with manual rerender

--- components/Boards/CustomMovesTestBoard.js
+++ components/Boards/CustomMovesTestBoard.js
@@ -12,7 +12,7 @@ export default function RandomMoveEngine() {
 
   //temporarily static player-names
   const playerName = "Player One";
-  const oppenentName = "RandomMoveMachine";
+  const oppenentName = "Player Two";
 
   // // ---CREATE GAME OBJECT---
   useEffect(() => {
@@ -31,16 +31,9 @@ export default function RandomMoveEngine() {
 
   // // ---MAKE A MOVE AND UPDATE GAME OBJECT---
   function makeAMove(move) {
-    // logs white and black "move" but only logs black previousMove!
-    // console.log("log move from within makeMove:");
-    // console.log(move, "––– move");
-    // console.log("log previousMove from within makeMove:");
-    // console.log(previousMove);
-
     const gameCopy = { ...game };
     const result = gameCopy.move(move);
 
-    //gain data for moveStatus and previousMove
     setPreviousMove((previousMove) => {
       if (previousMove?.flags === "c") {
         zombieMove(previousMove, { ...game });
@@ -69,57 +62,26 @@ export default function RandomMoveEngine() {
     const move = makeAMove({
       from: sourceSquare,
       to: targetSquare,
-      promotion: "q", // always promote to a queen for simplicity
+      promotion: "q",
     });
 
     if (move === null) return false; // check illegal move
 
-    //"thinking-time" before black RandomMoveEngine trigger
-    setTimeout(makeRandomMove, 1500);
-
-    // onDrop only logs white moves!
-    // console.log("onDrop:");
-    // console.log(move);
-
     return true;
   }
 
-  // // ---CREATE A RANDOM MOVE FOR BLACK + PASS MOVE TO makeAMove---
-  function makeRandomMove() {
-    const possibleMoves = game.moves();
-    //check game status (chess.js V1_beta onwards: .game_over() => .isGameOver() - ect)
-    if (game.game_over() || game.in_draw() || possibleMoves.length === 0) {
-      return; // exit if the game is over
-    }
-    const randomIndex = Math.floor(Math.random() * possibleMoves.length); //create random move
-    makeAMove(possibleMoves[randomIndex]); //trigger black makeAMove (only targetsquare)
-  }
-
-  // // ---CHECK FOR ZOMBIE PIECE => RESPAWN ZOMBIE => SET NEW GAME OBJECT WITH THROUGH .fen() ---
-  //  ---use previousMove to check captures (flags = "c")
-  console.log("OUTSIDE makeMove: log previousMove + fen");
   console.log(previousMove);
   console.log(game?.fen());
-  // useEffect(() => {
-  //   if (previousMove?.flags === "c") {
-  //     const newGame = new Chess(game.fen());
-  //     newGame.put(
-  //       { type: previousMove.captured, color: previousMove.color },
-  //       previousMove.from
-  //     );
-  //     setGame(newGame);
-  //   }
-  // }, [previousMove]);
-  // console.log("GAME");
 
   const gameConstellation = game ? game.fen() : null;
+
   return (
     <>
       <h2>
         TOTALLY <i>UNHINGED</i> CHESS
       </h2>
       {gameConstellation && (
-        <Chessboard position={gameConstellation} onPieceDrop={onDrop} />
+        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
       )}
       {moveStatus.gameOver && <GameTerminal moveStatus={moveStatus} />}
       {previousMove ? (
