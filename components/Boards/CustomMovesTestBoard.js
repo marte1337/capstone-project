3e06797556726a5f72d47763fe485a49daf0c6d4
Marte Auer <marte_auer@hotmail.com>
2023-05-10 12:18:45 +0200

    add working gamehistory

--- components/Boards/CustomMovesTestBoard.js
+++ components/Boards/CustomMovesTestBoard.js
@@ -7,8 +7,10 @@ import GameTerminal from "../GameTerminal";
 
 export default function RandomMoveEngine() {
   const [game, setGame] = useState(null);
+  const [fen, setFen] = useState(game?.fen());
   const [previousMove, setPreviousMove] = useState(null);
   const [moveStatus, setMoveStatus] = useState({});
+  const [history, setHistory] = useState([]);
 
   //temporarily static player-names
   const playerName = "Player One";
@@ -19,30 +21,50 @@ export default function RandomMoveEngine() {
     setGame(new Chess());
   }, []);
 
-  function zombieMove(previousMove, game) {
-    game.put(
-      { type: previousMove.captured, color: previousMove.color },
-      previousMove.from
-    );
-    const newGame = new Chess(game.fen());
-
-    setGame(newGame);
+  // // ---CREATE SEPERATE GAME HISTORY TO BYPASS GAME RESETS---
+  function historyStorage() {
+    if (
+      history[history.length - 1]?.color !==
+      game?.history({ verbose: true })[
+        game.history({ verbose: true }).length - 1
+      ]?.color
+    ) {
+      setHistory([
+        ...history,
+        game?.history({ verbose: true })[
+          game.history({ verbose: true }).length - 1
+        ],
+      ]);
+    }
   }
+
+  // // ---CHECK FOR ZOMBIE PIECE => RESPAWN ZOMBIE => SET NEW GAME OBJECT WITH .fen() ---
+  //  ---use previousMove to check captures (flags = "c")
+  useEffect(() => {
+    if (previousMove?.flags === "c") {
+      game.put(
+        { type: previousMove.captured, color: previousMove.color },
+        previousMove.from
+      );
+      const newGame = new Chess(game.fen());
+      setGame(newGame);
+      setFen(newGame.fen());
+      historyStorage();
+    }
+  }, [previousMove]);
 
   // // ---MAKE A MOVE AND UPDATE GAME OBJECT---
   function makeAMove(move) {
     const gameCopy = { ...game };
     const result = gameCopy.move(move);
+    // setGame(gameCopy);
+    setFen(gameCopy.fen());
 
-    setPreviousMove((previousMove) => {
-      if (previousMove?.flags === "c") {
-        zombieMove(previousMove, { ...game });
-        return undefined;
-      }
-      return gameCopy.history({ verbose: true })[
+    setPreviousMove(
+      gameCopy.history({ verbose: true })[
         gameCopy.history({ verbose: true }).length - 1
-      ];
-    });
+      ]
+    );
 
     setMoveStatus({
       moveNumber: game.history().length,
@@ -53,36 +75,31 @@ export default function RandomMoveEngine() {
       isStalemate: game.in_stalemate(),
       gameOver: game.game_over(),
     });
+
+    historyStorage();
+
     return result; // null if the move was illegal, the move object if the move was legal
   }
 
   // // ---ON DROP = PASS WHITE MOVE TO makeAMove + TRIGGER BLACK MOVE CREATION
   function onDrop(sourceSquare, targetSquare) {
-    //trigger white makeAMove (sourcesquare, targetsquare, promotion)
     const move = makeAMove({
       from: sourceSquare,
       to: targetSquare,
       promotion: "q",
     });
-
     if (move === null) return false; // check illegal move
-
     return true;
   }
 
-  console.log(previousMove);
-  console.log(game?.fen());
-
-  const gameConstellation = game ? game.fen() : null;
+  console.log(history);
 
   return (
     <>
       <h2>
         TOTALLY <i>UNHINGED</i> CHESS
       </h2>
-      {gameConstellation && (
-        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
-      )}
+      {game && <Chessboard position={fen} onPieceDrop={onDrop} />}
       {moveStatus.gameOver && <GameTerminal moveStatus={moveStatus} />}
       {previousMove ? (
         <MoveInfo previousMove={previousMove} moveStatus={moveStatus} />
