e4531532cdee01a09199f62aecf6298ed007b451
Marte Auer <marte_auer@hotmail.com>
2023-05-15 15:13:22 +0200

    add styled component wrapper

---
+++ components/BoardWrapper.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export default function BoardWrapper({ children }) {
+  return (
+    <StyledSection>
+      <StyledDiv>{children}</StyledDiv>
+    </StyledSection>
+  );
+}
+
+const StyledSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledDiv = styled.div`
+  width: 80%;
+  max-width: 800px;
+  padding: 0.5rem 0;
+`;
--- pages/singleplayer.js
+++ pages/singleplayer.js
@@ -1,24 +1,10 @@
-import styled from "styled-components";
 import PlayerVsRandomMove from "@/components/Boards/PlayerVsRandomMove";
+import BoardWrapper from "@/components/BoardWrapper";
 
 export default function SinglePlayerPage() {
   return (
-    <StyledSection>
-      <StyledDiv>
-        <PlayerVsRandomMove />
-      </StyledDiv>
-    </StyledSection>
+    <BoardWrapper>
+      <PlayerVsRandomMove />
+    </BoardWrapper>
   );
 }
-
-const StyledSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledDiv = styled.div`
-  width: 80%;
-  max-width: 800px;
-  padding: 0.5rem 0;
-`;
