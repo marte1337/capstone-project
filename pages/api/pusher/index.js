97fd617fc33c525a3b8d3e0bc25024a0f68ada5e
Marte Auer <marte_auer@hotmail.com>
2023-05-16 17:19:58 +0200

    add changes

--- pages/api/pusher/index.js
+++ pages/api/pusher/index.js
@@ -1,12 +1,27 @@
 import { pusher } from "../../../lib/pusher";
 
-// public channel handler
+// presence channel handler
 export default async function handler(req, res) {
-  const { message, sender } = req.body;
-  await pusher.trigger("chat", "chat-event", {
+  const { message, username } = req.body;
+
+  // trigger a new post event via pusher
+  await pusher.trigger("presence-channel", "chat-update", {
     message,
-    sender,
+    username,
   });
 
-  res.json({ message: "completed" });
+  res.json({ status: 200 });
 }
+
+// import { pusher } from "../../../lib/pusher";
+
+// // public channel handler
+// export default async function handler(req, res) {
+//   const { message, sender } = req.body;
+//   await pusher.trigger("chat", "chat-event", {
+//     message,
+//     sender,
+//   });
+
+//   res.json({ message: "completed" });
+// }
---
+++ pages/lobby.js
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Pusher from "pusher-js";
+import axios from "axios";
+
+export default function Lobby({ username }) {
+  const router = useRouter();
+  const pusher = new Pusher(process.env.NEXT_PUBLIC_KEY, {
+    cluster: "eu",
+    // use jwts in prod
+    authEndpoint: `api/pusher/auth`,
+    auth: { params: { username } },
+  });
+
+  const [chats, setChats] = useState([]);
+  const [messageToSend, setMessageToSend] = useState("");
+  const [onlineUsersCount, setOnlineUsersCount] = useState(0);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [usersRemoved, setUsersRemoved] = useState([]);
+
+  useEffect(() => {
+    const channel = pusher.subscribe("presence-channel");
+
+    // when a new member successfully subscribes to the channel
+    channel.bind("pusher:subscription_succeeded", (members) => {
+      // subscribed members count
+      setOnlineUsersCount(members.count);
+    });
+
+    // when a new member joins the chat
+    channel.bind("pusher:member_added", (member) => {
+      // console.log("count",channel.members.count)
+      setOnlineUsersCount(channel.members.count);
+      setOnlineUsers((prevState) => [
+        ...prevState,
+        { username: member.info.username },
+      ]);
+    });
+
+    // when a member leaves the chat
+    channel.bind("pusher:member_removed", (member) => {
+      setOnlineUsersCount(channel.members.count);
+      setUsersRemoved((prevState) => [...prevState, member.info.username]);
+    });
+
+    // updates chats
+    channel.bind("chat-update", function (data) {
+      const { username, message } = data;
+      setChats((prevState) => [...prevState, { username, message }]);
+    });
+
+    // when closing channel, unsubscribe user
+    return () => {
+      pusher.unsubscribe("presence-channel");
+    };
+  }, []);
+
+  const handleSignOut = () => {
+    pusher.unsubscribe("presence-channel");
+    router.push("/");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("/api/pusher/chat-update", {
+      message: messageToSend,
+      username,
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <p>
+          Hello, <span>{sender}</span>
+        </p>
+        <div>
+          <button onClick={handleSignOut}>Sign out</button>
+        </div>
+        <div>{onlineUsersCount} user(s) online now</div>
+
+        <h2>Notifications</h2>
+        <div>
+          {/* show online users */}
+          {onlineUsers.map((user, id) => (
+            <div key={id}>
+              <small>
+                {" "}
+                <span>{user.username}</span> just joined the chat
+              </small>
+            </div>
+          ))}
+
+          {/* show users leaving the chat */}
+          {usersRemoved.map((user, id) => (
+            <small key={id}>
+              {" "}
+              <span>{user}</span> just left the chat!
+            </small>
+          ))}
+        </div>
+
+        <div>
+          {chats.map((chat, id) => (
+            <div key={id}>
+              <p>{chat.message}</p>
+              <small>{chat.username}</small>
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={messageToSend}
+              onChange={(e) => setMessageToSend(e.target.value)}
+              handleSubmit={(e) => {
+                handleSubmit(e);
+              }}
+              placeholder="start typing...."
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
