8f699f9b40962081153966d7c219709f13457ff8
marte1337 <54943747+marte1337@users.noreply.github.com>
2023-07-11 12:23:00 +0200

    add header component

---
+++ components/styles/ImageStyles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const StyledImage = styled(Image)`
+  border: solid black 0.3rem;
+  border-radius: 5px;
+  box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.5);
+  margin-top: 0.8rem;
+`;
